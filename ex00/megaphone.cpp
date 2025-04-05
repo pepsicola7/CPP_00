c973fef64d075f6dadcf8a2a871da0e25b02c580
Peiqi <perrine0624@gmail.com>
2025-04-05 11:19:14 +0200

    finish 00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 #include <cctype>
 
-void *uppercase_str(std::string argv)
+void uppercase_str(std::string argv)
 {
 	int	i;
 
 	i = 0;
 	while (argv[i])
 	{
-		if (argv[i] != """")
+		if (argv[i] != '"')
 		{
 			argv[i] = std::toupper(argv[i]);
-			std::cout << argv[i] << 
+			std::cout << argv[i];
 		}
 		else 
 			i++;
 		i++;
 	}
-	return (argv);
+	return ;
 }
 
 int	main(int argc, char *argv[])
 {
 	int		i;
-	char	*str;
+
 	if (argc < 0)
 		return (1);
 	if (argc == 0)
@@ -33,11 +33,14 @@ int	main(int argc, char *argv[])
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	i = 0;
-	while (i != argc - 1)
+	i = 1;
+	while (i < argc)
 	{
 		uppercase_str(std::string(argv[i]));
+		if (i != argc - 1)
+			std::cout << " " ;
 		i++;
 	}
+	std :: cout << std :: endl;
 	return (0);
 }
